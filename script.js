3548ba82d7bb594693ee492dff5ff402064f023e
Duke <aboukhodr21@gmail.com>
2022-10-17 11:17:48 -0400

    calculator is working

--- script.js
+++ script.js
@@ -1,60 +1,50 @@
 let displayValue = ""; 
-let currentValue = ""; 
-let newValue = "";
-// const operator = ["+", "-", "/", "*"]
+let leftOperand = ""; 
+let rightOperand = "";
+let operator = ""
 
 const operations = document.querySelectorAll('.operation')
 for (let i = 0; i < operations.length; i++) {
     operations[i].addEventListener('click', function(e) { 
         console.log(e.target.id)
-        currentValue = displayValue;
+        operator = e.target.id 
+        leftOperand = displayValue;
         displayValue = "";
         return e.target.id
     });
 }
 
-function operate(displayValue,newValue) { 
-    let result = `${displayValue} ${newValue}`
-    return result
-};
+function operate(leftOperand,rightOperand,operator) { 
+    let a = parseInt(leftOperand)
+    let b = parseInt(rightOperand)
+    if (operator === "+") return a + b;
+    if (operator === "*") return a * b;
+    if (operator === "/") return a / b;
+    if (operator === "-") return a - b; 
+    /*Throw error*/
+}
 
-const equalSign = document.querySelector('#equalSign')
+const equalSign = document.querySelector('.equalSign')
 equalSign.addEventListener('click', function(e){
-    console.log(e.target.id)
-    let displayValue = newValue;
-    let result = currentValue e.target.id newValue
-    console.log(result)
-    return result
+    // console.log(operator)
+    // console.log(e.target.id)
+    console.log(rightOperand)
+    rightOperand = displayValue
+    let resultant = operate(leftOperand,rightOperand, operator)
+    console.log('leftOperand = ' + leftOperand)
+    console.log('rightOperand = ' + rightOperand)
+    console.log('operator = ' + operator)
+    console.log('resultant = ' + resultant)
+
 });
 
 const numberButton = document.querySelectorAll('.numberButton')
 for (i = 0; i < numberButton.length; i++) { 
     numberButton[i].addEventListener('click', function(e) {
-        console.log(e.target.id)
         displayValue = displayValue + e.target.id
-        console.log(displayValue)
     });
 }; 
-console.log(displayValue)
 
 
-// const add = function(displayValue,newValue) {
-    // 	let result = a + b
-    //   return result
-    // };
-    
-    // const subtract = function(displayValue,newValue) {
-    // 	let result = a - b 
-    //   return result
-    // };
-    
-    // const sum = function(displayValue,newValue) {
-    //   let result = a + b 
-    //   return result
-        
-    // };
-    
-    // const multiply = function(displayValue,newValue) {
-    //   let result = a * b 
-    //   return result
-    // 
+
+
